cb633cea545113c6d293be31cf09c79061a35fbc
Caio Barbosa <caiomontebarbosa104@gmail.com>
2024-10-18 22:47:29 -0300

    Atividade28

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import Filho from "./Filho"
+
+function Card(){
+    return <div className="card">
+        <Filho/>
+    </div>
+}
+
+export default Card
---
+++ src/components/Filho.jsx
@@ -0,0 +1,14 @@
+function Filho(){
+    return(
+        <>
+        <h2>Colocando texto atráves de outro componente</h2>
+
+        <hr />
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nihil impedit, laborum ea, neque ducimus rem maxime quibusdam ipsa consectetur quasi aut, consequuntur possimus dolor! Maxime accusantium cupiditate voluptate itaque!</p>
+        </>
+    )
+}
+
+
+export default Filho
